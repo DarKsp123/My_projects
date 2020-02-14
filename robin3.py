74db241857b02fca7a1450b36874f31a72634427
DarKsp123 <kirillvolodin90@yandex.ru>
2020-02-14 09:37:39 +0300

    My first_way to Python World! Add work with rows and other functions of type string! Work with dictionary, check attributes in dictionary, Work with cycle for, files, binary numbers and others The end of introduction and the begining of the swim Every thing is ok now Work with numbers(quantitys) Continued The strings continued Strings metods, formating metods The end of Strings and the begining of Lists The end of List and the begining of dictionary To continue working with the dictionary The end working with Dictionary, begins work with Cortege and Files Begins with Operators Learns about operator if, try and cycle while:

--- robin3.py
+++ robin3.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python3
 print('Run', 'away!...')
+                                    # Обработка ошибок с помощью оператора try
+print('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Обработка ошибок с помощью оператора try')
+while True:                         # начало цикла
+    reply = input('Enter text: ')   # Вввод данных пользователя
+    if reply == 'stop': break       # Если ввести стоп, закрыть программу
+    try:                            # Оператор обработки исключений
+        num = int(reply)            # Проверка ввода пользователя на число
+    except:                         # Вызов исключения для случаев если пользователь ввел не число
+        print('Bad!' * 8)           # Печатаем то что в принте
+    else:                           # А если число, то возводим его в квадрат
+        print(num ** 2)
+print('Bye')
+
+                                        # Вложение кода на три уровня в глубину
+print('\n\t\t\t\t\t\t\t\t\t\t\t\t\t Вложение когда на три уровня в глубину')
+
+while True:
+    reply = input('Введите текст: ')
+    if reply == 'stop':
+        break
+    elif not reply.isdigit():
+        print('Bad!' * 8)
+    else:
+        num = int(reply)
+        if num <= 20:
+            print('low')
+        else:
+            print(num ** 2)
+print('Bye')
