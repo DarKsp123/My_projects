538d5e2c12186dead8c6ad9e7abb60397cce00dc
DarKsp123 <kirillvolodin90@yandex.ru>
2020-10-19 19:05:32 +0300

    My first_way to Python World! Add work with rows and other functions of type string! Work with dictionary, check attributes in dictionary, Work with cycle for, files, binary numbers and others The end of introduction and the begining of the swim Every thing is ok now Work with numbers(quantitys) Continued The strings continued Strings metods, formating metods The end of Strings and the begining of Lists The end of List and the begining of dictionary To continue working with the dictionary The end working with Dictionary, begins work with Cortege and Files Begins with Operators Learns about operator if, try and cycle while: First program On Python Begins work with operators Python Work with operator "print" End work with if End work with cycles while, for, else End work with chapter 13 and with cycles. It's begin chapter 14 - iteration and includes. To continue iteration and list includes The end of Chapter 3. Begins to do the home task of this chapter.

--- lesson_003/Probe_1.py
+++ lesson_003/Probe_1.py
@@ -29,3 +29,68 @@ if __name__ == '__main__':
     assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
     assert nearest_value({-1, 2, 3}, 0) == -1
     print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def nearest_value(values: set, one: int) -> int:
+    values = sorted(values)
+    diffs = list([abs(int - one) for int in values])  # create a list of differences
+    return values[diffs.index(min(diffs))]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def nearest_value(values: set, one: int) -> int:
+    values = sorted(values)
+    temp = values[0]
+    for i in values:
+        if abs(i - one) < abs(temp - one):
+            temp = i
+    return temp
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+def nearest_value(values: set, one: int) -> int:
+    return min(sorted(values), key=lambda x:  max(x, one) - min(x, one))
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    assert nearest_value([0,-2],-1) == -2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
